b06a24f6cee2319acfe89ea1abc81185141546c2
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-10-18 13:20:15 -0500

    added handleChange and handleSubmit functions

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -7,22 +7,34 @@ export default function SignUp() {
   const [values, setValues] = useState(initialValues)
   const [errors, setErrors] = useState(initialErrors)
 
+  const handleChange = (event) => {
+    console.log(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    console.log('submitted')
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label>
         Username
-        <input type='text' />
+        <input type='text' value={values.username} onChange={handleChange} />
       </label>
 
       <label>
         Phone Number
-        <input type='tel' />
+        <input type='tel' value={values.tel} onChange={handleChange} />
       </label>
 
       <label>
         Password
-        <input type='password' />
+        <input type='password' value={values.password} onChange={handleChange} />
       </label>
+
+      <button>Signup</button>
     </form>
   )
 }
