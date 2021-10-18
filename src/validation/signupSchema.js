7c8a36558939306b986d0318dfdd21e6ee78332d
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-10-18 13:47:17 -0500

    added yup and signupSchema for validating signup form input

---
+++ src/validation/signupSchema.js
@@ -0,0 +1,15 @@
+import * as yup from 'yup'
+
+export const schema = yup.object().shape({
+  username: yup()
+    .string()
+    .trim()
+    .required('Username is required')
+    .min(4, 'Username must be a least 4 characters long'),
+  tel: yup()
+    .string()
+    .required('Phone number is required')
+    .test('len', 'Must be a valid 10-digit phone number', (val) => val.length === 10)
+    .matches(/^[0-9]/, 'Phone number is not valid'),
+  password: yup().string().required('Password is required'),
+})
