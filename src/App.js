36e91d68b5555b8b144aa21efcaa42059326cfa5
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-10-18 13:13:58 -0500

    added signup form structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import SignupForm from './components/SignupForm'
+
 export default function App() {
   return (
     <div>
       <p>Hello, world</p>
+      <SignupForm />
     </div>
   )
 }
---
+++ src/components/SignupForm.js
@@ -0,0 +1,20 @@
+export default function SignUp() {
+  return (
+    <form>
+      <label>
+        Username
+        <input type='text' />
+      </label>
+
+      <label>
+        Phone Number
+        <input type='tel' />
+      </label>
+
+      <label>
+        Password
+        <input type='password' />
+      </label>
+    </form>
+  )
+}
