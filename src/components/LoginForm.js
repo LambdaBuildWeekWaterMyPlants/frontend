cb6b969c5c55f41fbe7aae24649a213463aa9810
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-10-18 21:08:01 -0500

    added state to login form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,7 +2,14 @@ import { useState, useEffect } from 'react'
 import * as yup from 'yup'
 import { loginSchema as schema } from '../validation'
 
+const initialValues = { username: '', password: '' }
+const initialErrors = { username: '', password: '' }
+
 export default function LoginForm({ submit }) {
+  const [values, setValues] = useState(initialValues)
+  const [errors, setErrors] = useState(initialErrors)
+  const [disabled, setDisabled] = useState(false)
+
   const handleSubmit = (event) => {}
 
   return (
