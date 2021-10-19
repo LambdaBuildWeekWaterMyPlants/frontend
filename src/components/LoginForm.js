9d1a94d0c91f7c8bb9f4cd480f9eb088f1cd0eaa
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-10-18 21:08:56 -0500

    added validate function

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -5,10 +5,24 @@ import { loginSchema as schema } from '../validation'
 const initialValues = { username: '', password: '' }
 const initialErrors = { username: '', password: '' }
 
+// receives a callback function in props names submit
 export default function LoginForm({ submit }) {
   const [values, setValues] = useState(initialValues)
   const [errors, setErrors] = useState(initialErrors)
   const [disabled, setDisabled] = useState(false)
+
+  // validates input using yup and schema
+  const validate = (name, value) => {
+    yup
+      .reach(schema, name)
+      .validate(value)
+      .then(() => {
+        setErrors((prev) => ({ ...prev, [name]: '' }))
+      })
+      .catch((err) => {
+        setErrors((prev) => ({ ...prev, [name]: err.errors[0] }))
+      })
+  }
 
   const handleSubmit = (event) => {}
 
