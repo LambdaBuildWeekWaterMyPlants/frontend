c1af95d7846b82894c52248bd1e71e33ae633aa2
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-10-18 21:03:26 -0500

    renamed folders to be lower cased

--- src/Validation/LoginSchema.js
+++
@@ -1,9 +0,0 @@
-import * as yup from 'yup'
-export const schema = yup.object().shape({
-  username: yup
-    .string()
-    .trim()
-    .required('Username is a required field')
-    .min(4, 'Username min length must be 4characters'),
-  password: yup.string().required('Password of any length required'),
-})
