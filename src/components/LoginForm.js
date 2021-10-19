2974e40b6a1827a2584aa23556ad07b4191ca862
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-10-18 21:07:17 -0500

    updated login form structure

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,4 +2,28 @@ import { useState, useEffect } from 'react'
 import * as yup from 'yup'
 import { loginSchema as schema } from '../validation'
 
-export default function LoginForm({ submit }) {}
+export default function LoginForm({ submit }) {
+  const handleSubmit = (event) => {}
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Sign In</h2>
+
+      <div>
+        <label>
+          Username
+          <input type='text' />
+        </label>
+      </div>
+
+      <div>
+        <label>
+          Password
+          <input type='password' />
+        </label>
+      </div>
+
+      <button>Login</button>
+    </form>
+  )
+}
