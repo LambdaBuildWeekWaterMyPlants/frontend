7f62845b3a77732aac0af6140402dd0f1deb543e
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-10-20 04:53:26 -0500

    added router and styled components to index.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,8 @@ export default function Header() {
   return (
     <header>
       <h1>{config.title}</h1>
+
+      <nav></nav>
     </header>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
 
 import App from './App'
+import { theme } from './theme'
 import './index.css'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <ThemeProvider theme={theme}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ThemeProvider>
+  </Router>,
   document.getElementById('root')
 )
