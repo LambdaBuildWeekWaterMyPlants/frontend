bfc706ee40cfed4869afa402f0e1e26478ce6cc8
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-10-20 05:11:02 -0500

    added routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
+import { useState } from 'react'
 import { Route } from 'react-router-dom'
 import Layout from './components/Layout'
 
+import Home from './pages/home'
+import Login from './pages/login'
+import Signup from './pages/signup'
+import Update from './pages/update'
+
 export default function App() {
-  return <Layout></Layout>
+  const [user, setUser] = useState(null)
+
+  return (
+    <Layout user={user}>
+      <Route exact path='/'>
+        <Home />
+      </Route>
+      <Route path='/login'>
+        <Login />
+      </Route>
+      <Route path='/signup'>
+        <Signup />
+      </Route>
+      <Route path='/update'>
+        <Update />
+      </Route>
+    </Layout>
+  )
 }
